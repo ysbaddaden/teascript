69cfaed14de80982edd1d6823288ea30d7e5029c
Julien Portalier <ysbaddaden@gmail.com>
2011-11-14 10:51:32 +0100

    Tests for array slice with ranges.

---
+++ test/types/range_test.js
@@ -0,0 +1,29 @@
+var assert = require("assert");
+var T = require("../../lib/tea");
+
+exports.testExclusiveSlice = function () {
+  assert.equal("Array.prototype.slice.call([ 1, 2, 3 ], 1, 4);",
+    T.toJavaScript("[1, 2, 3][1...4]"));
+  
+  assert.equal("var ary;\nArray.prototype.slice.call(ary, 0, 4);",
+    T.toJavaScript("ary[0...4]"));
+  
+  assert.equal("var ary;\nArray.prototype.slice.call(ary, 3, ary.length);",
+    T.toJavaScript("ary[3...ary.length]"));
+}
+
+exports.testInclusiveSlice = function () {
+  assert.equal("Array.prototype.slice.call([ 1, 2, 3 ], 1, 5);",
+    T.toJavaScript("[1, 2, 3][1..4]"));
+  
+  assert.equal("var ary;\nArray.prototype.slice.call(ary, 0, 5);",
+    T.toJavaScript("ary[0..4]"));
+  
+  assert.equal("var ary;\nArray.prototype.slice.call(ary, ary[0], ary.length + 1);",
+    T.toJavaScript("ary[ary[0]..ary.length]"));
+}
+
+if (module === require.main) {
+  require("../test").run(exports);
+}
+
