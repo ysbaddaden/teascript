44b47fde0f8031e918ac0454b8063c258d3d63d2
Julien Portalier <ysbaddaden@gmail.com>
2011-11-17 10:13:16 +0100

    Tests for loop.

---
+++ test/statements/loop_statement_test.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+var T = require("../../lib/tea");
+
+exports.testEmptyLoopStatement = function () {
+  assert.equal("while (true) {\n\n}", T.toJavaScript("loop do\nend"));
+  assert.equal("while (true) {\n\n}", T.toJavaScript("loop do;end"));
+}
+
+exports.testLoopStatement = function () {
+  assert.equal("while (true) {\n" +
+    "    doSomething();\n" +
+    "}", T.toJavaScript("loop\ndoSomething()\nend"));
+  
+  assert.equal("var x;\n" +
+    "while (true) {\n" +
+    "    if (x > 1) {\n" +
+    "        break;\n" +
+    "    } else {\n" +
+    "        x += 1;\n" +
+    "    }\n" +
+    "}", T.toJavaScript("loop do;if x > 1;break;else x+= 1;end;end"));
+}
+
+if (module === require.main) {
+  require("../test").run(exports);
+}
