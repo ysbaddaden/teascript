85b826b877536388c15abbcb8a67b535612e4e28
Julien Portalier <julien@portalier.com>
2013-02-03 11:25:01 +0100

    Correct unary expressions for +, - and ~.

--- test/expressions/unary_expression_test.js
+++ test/expressions/unary_expression_test.js
@@ -1,6 +1,23 @@
 var assert = require("assert");
 var T = require("../../lib/tea");
 
+exports.testMinus = function () {
+  assert.equal("-1;", T.toJavaScript("-1"));
+  assert.equal("-doSomething();", T.toJavaScript("-doSomething()"));
+  assert.equal("var x;\nx = -(e.pageX + div.scrollTop);",
+      T.toJavaScript("x = -(e.pageX + div.scrollTop)"));
+};
+
+exports.testPlus = function () {
+  assert.equal("+1;", T.toJavaScript("+1"));
+  assert.equal("+doSomething();", T.toJavaScript("+doSomething()"));
+};
+
+exports.testTilde = function () {
+  assert.equal("~1.5e10;", T.toJavaScript("~1.5e10"));
+  assert.equal("~doSomething();", T.toJavaScript("~doSomething()"));
+};
+
 exports.testNot = function () {
   assert.equal("var bool;\nbool = !true;", T.toJavaScript("bool = !true"));
   assert.equal("var b;\nb = !doSomething();", T.toJavaScript("b = !doSomething()"));
