61116e62ecb770c9448e7e54bddfabf25fa056cc
Julien Portalier <julien@portalier.com>
2013-02-20 15:02:15 +0100

    Tests + fix for return statement.

---
+++ test/statements/return_statement_test.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+var T = require("../../build/tea");
+
+exports["test empty return statement"] = function () {
+  assert.equal("return;", T.compile("return"));
+  assert.equal("return;", T.compile("return;"));
+};
+
+exports["test return statement with an argument"] = function () {
+  assert.equal("return process;", T.compile("return process"));
+  assert.equal("return argv[0];", T.compile("return argv[0];"));
+};
+
+if (module === require.main) {
+  require("../test").run(exports);
+}
